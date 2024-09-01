1e6ac6202b1bf774f0648dbe0b63078e3149cc45
AaronHsieh <piafyoyo06@gmail.com>
2024-09-02 02:10:03 +0800

    當使用者來到首頁就直接導向到 login 頁面

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true, // 設為 true 表示永久重定向 (301)，設為 false 表示臨時重定向 (307)
+      },
+    ];
+  },
+};
 
 export default nextConfig;
