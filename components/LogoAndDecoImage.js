df3a5b5ea88a859b7bcf2d1cf10c2f66132d060f
AaronHsieh <piafyoyo06@gmail.com>
2024-09-02 01:07:40 +0800

    fix: 修正圖片路徑

--- components/LogoAndDecoImage.js
+++ components/LogoAndDecoImage.js
@@ -6,13 +6,13 @@ export default function LogoAndDecoImage() {
     <aside className={styles.aside}>
       <Link href="/login">
         <img
-          src="../public/image/logo.webp"
+          src="/image/logo.webp"
           alt="網站 Logo"
           className={styles.logo}
         />
       </Link>
       <img
-        src="../public/image/login-and-register.webp"
+        src="/image/login-and-register.webp"
         alt="登入註冊頁面裝飾圖片"
         className={styles.hideOnMobile}
       />
