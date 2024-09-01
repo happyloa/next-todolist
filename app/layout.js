95705bcd7368bcfdf5634e00b8989affa3a76259
AaronHsieh <piafyoyo06@gmail.com>
2024-09-02 02:07:27 +0800

    在 layout 檔案中使用 og 圖片

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,16 @@ const notoSansTC = Noto_Sans_TC({
 export const metadata = {
   title: "Next.js To-Do List",
   description: "Next.js To-Do List refactored from Vue3 by Aaron",
+  openGraph: {
+    images: [
+      {
+        url: "/og-image.webp", // 用於社交媒體分享時顯示的圖片 URL
+        width: 1200, // 圖片的寬度
+        height: 630, // 圖片的高度
+        alt: "Next.js 14 To-Do List", // 圖片的替代文字
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
