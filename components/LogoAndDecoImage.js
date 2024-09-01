29c9a2ee5f9b8c3f2b1f751619f4a8011e15c554
AaronHsieh <piafyoyo06@gmail.com>
2024-09-02 01:01:23 +0800

    fix: 修正樣式檔案路徑

--- components/LogoAndDecoImage.js
+++ components/LogoAndDecoImage.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles/LogoAndDecoImage.module.css";
+import styles from "./LogoAndDecoImage.module.css";
 
 export default function LogoAndDecoImage() {
   return (
